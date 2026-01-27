65489cb245069f5745aee1c3f55a381cf713a4b1
kyuhancs <hankyumin05@gmail.com>
2026-01-27 21:37:47 +0000

    created new files and applications

--- app/page.js
+++ app/page.js
@@ -2,7 +2,21 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
+      <h1>kyu han website</h1>
+
+      <a
+        className="social-tab"
+        href="https://www.instagram.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="social-icon"
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+          alt="Instagram"
+        />
+        <span>Instagram</span>
+      </a>
     </div>
   );
 }
