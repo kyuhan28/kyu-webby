a7f6c6edee64b80baa593fdf447bba4adb074a42
kyuhancs <hankyumin05@gmail.com>
2026-01-27 21:38:11 +0000

    created new code and website

--- app/page.js
+++ app/page.js
@@ -2,11 +2,19 @@
 export default function Home() {
   return (
     <div>
-      <h1>kyu han website</h1>
+      <img
+        className="profile-icon"
+        src="/charmander.png"
+        alt="Profile Icon"
+      />
+
+      <div className="name-box">Kyu Han</div>
+      <div className="subtitle-box">University of Pittsburgh</div>
+      <div className="description-box">Class of 2028</div>
 
       <a
         className="social-tab"
-        href="https://www.instagram.com/"
+        href="https://www.instagram.com/kyuhannnnn"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -15,8 +23,39 @@ export default function Home() {
           src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
           alt="Instagram"
         />
-        <span>Instagram</span>
       </a>
+
+      <a
+        className="social-tab social-tab-second"
+        href="https://twitter.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="social-icon"
+          src="/charmander.png"
+          alt="Twitter"
+        />
+      </a>
+
+      <a
+        className="social-tab social-tab-third"
+        href="https://github.com/kyuhan28"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="social-icon"
+          src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+          alt="GitHub"
+        />
+      </a>
+
+      <div className="about-me-box">
+        <h2>About Me</h2>
+        <p>pop</p>
+      </div>
+
     </div>
   );
 }
