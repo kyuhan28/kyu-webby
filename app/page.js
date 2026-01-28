dd164b03bb4afb189a482d7486b13d65ca0f901d
kyuhancs <hankyumin05@gmail.com>
2026-01-28 06:34:53 +0000

    made more changes

--- app/page.js
+++ app/page.js
@@ -1,60 +1,136 @@
-// pages/index.js or app/page.js
 export default function Home() {
   return (
     <div>
-      <img
-        className="profile-icon"
-        src="/charmander.png"
-        alt="Profile Icon"
-      />
+      <nav className="top-nav">
+        <a href="#home" className="nav-link">Home</a>
+        <a href="#about" className="nav-link2">About</a>
+        <a href="#portfolio" className="nav-link3">Portfolio</a>
+        <a href="#experience" className="nav-link4">Experience</a>
+        <a href="#hobbies" className="nav-link5">Hobbies</a>
+        <a href="#contact" className="nav-link6">Contact</a>
+      </nav>
 
-      <div className="name-box">Kyu Han</div>
-      <div className="subtitle-box">University of Pittsburgh</div>
-      <div className="description-box">Class of 2028</div>
-
-      <a
-        className="social-tab"
-        href="https://www.instagram.com/kyuhannnnn"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      {/* Home Section */}
+      <section id="home" className="section">
         <img
-          className="social-icon"
-          src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
-          alt="Instagram"
-        />
-      </a>
-
-      <a
-        className="social-tab social-tab-second"
-        href="https://twitter.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          className="social-icon"
+          className="profile-icon"
           src="/charmander.png"
-          alt="Twitter"
+          alt="Profile Icon"
         />
-      </a>
 
-      <a
-        className="social-tab social-tab-third"
-        href="https://github.com/kyuhan28"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          className="social-icon"
-          src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-          alt="GitHub"
-        />
-      </a>
+        <div className="name-box">Kyu Han</div>
+        <div className="subtitle-box">University of Pittsburgh</div>
+        <div className="description-box">Class of 2028</div>
 
-      <div className="about-me-box">
-        <h2>About Me</h2>
-        <p>pop</p>
-      </div>
+        <a
+          className="social-tab"
+          href="https://www.instagram.com/kyuhannnnn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="social-icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+            alt="Instagram"
+          />
+        </a>
+
+        <a
+          className="social-tab social-tab-second"
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="social-icon"
+            src="/charmander.png"
+            alt="Twitter"
+          />
+        </a>
+
+        <a
+          className="social-tab social-tab-third"
+          href="https://github.com/kyuhan28"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="social-icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+            alt="GitHub"
+          />
+        </a>
+      </section>
+
+      {/* About Me Section */}
+      <section id="about" className="section">
+        <div className="about-me-box">
+          <h2>About Me</h2>
+          <p> more here</p>
+        </div>
+      </section>
+
+      {/* Portfolio Section */}
+      <section id="portfolio" className="section">
+        <div className="content-box">
+          <h2>Portfolio</h2>
+          <p>description here</p>
+          <div className="project-item">
+            <h3>Deployed a Website with Vercel</h3>
+            <p>more here</p>
+          </div>
+          <div className="project-item">
+            <h3>Project 2</h3>
+            <p>none yet</p>
+          </div>
+          <div className="project-item">
+            <h3>Project 3</h3>
+            <p>none yet</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Experience Section */}
+      <section id="experience" className="section">
+        <div className="content-box">
+          <h2>Experience</h2>
+          <p>fun stuff</p>
+          <div className="experience-item">
+            <h3>non</h3>
+            <p>none yet</p>
+          </div>
+          <div className="experience-item">
+            <h3>none</h3>
+            <p>none yet</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Personal Hobbies Section */}
+      <section id="hobbies" className="section">
+        <div className="content-box">
+          <h2>Personal Hobbies</h2>
+          <p>desc here</p>
+          <div className="experience-item">
+            <h3>none</h3>
+            <p>none yet</p>
+          </div>
+          <div className="experience-item">
+            <h3>none</h3>
+            <p>none yet</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Contact Section */}
+      <section id="contact" className="section">
+        <div className="content-box">
+          <h2>Contact</h2>
+          <p>Feel free to reach out to me!</p>
+          <p>Email: hankyumin05@gmail.com</p>
+          <p>Phone: (856) 379-5682</p>
+        </div>
+      </section>
 
     </div>
   );
